df18b45c8d0a91e3cc1151d8e99d3facb40a6e94
monirul10491 <monirul10491@gmail.com>
2018-03-25 01:45:49 +0530

    All solutions added

---
+++ machine-learning-ex6/ex6/ex6New.m
@@ -0,0 +1,12 @@
+clc,clear,close all;
+fprintf('Loading and Visualizing Data ...\n')
+
+% Load from ex6data1: 
+% You will have X, y in your environment
+load('ex6data1.mat');
+
+% Plot training data
+plotData(X, y);
+
+fprintf('Program paused. Press enter to continue.\n');
+pause;
